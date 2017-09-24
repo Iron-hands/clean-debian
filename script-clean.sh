82030eb835cc4ddaf583b2f8d7d26d111993570e
Iron-hands <32245731+Iron-hands@users.noreply.github.com>
2017-09-24 15:04:01 -0300

    clean-script.sh
    use and root

---
+++ script-clean.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+##script para purger joguinhos e outros sem utilidades
+
+apt-get purge gnome-mines -y
+apt-get purge brasero -y
+apt-get purge cheese -y
+apt-get purge gnome-bluetooth -y
+apt-get purge gnome-chess -y
+apt-get purge gnome-tetravex -y
+apt-get purge tali -y 
+apt-get purge gnome-dictionary -y
+apt-get purge gnome-maps -y
+apt-get purge gnome-sudoku -y
+apt-get purge hitori -y 
+apt-get purge empathy -y
+apt-get purge evolution -y
+apt-get purge xboard -y
+apt-get purge gnome-sudoku -y
+apt-get purge gnome-klotski -y
+apt-get purge quadrapassel -y
+apt-get purge swell-foop -y
+apt-get purge lightsoff -y
+apt-get purge gnome-nibbles -y
+apt-get purge iagno -y
+apt-get purge gnome-mahjongg -y
+apt-get purge aisleriot -y
+apt-get purge inkscape -y
+apt-get purge transmission -y
+apt-get purge mutt -y
+apt-get purge four-in-a-row -y
+apt-get purge gnome-taquin -y
+apt-get purge gnome-robots -y
+apt-get autoclean -y
+apt-get clean -y
+echo "REMOVEU TUDO COM SUCESSO"
